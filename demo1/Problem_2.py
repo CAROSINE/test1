a0d43e2303e464e4e11c025dca336afcdc154546
CAROSINE <moshiur287rahman@gmail.com>
2025-08-12 21:52:34 +0600

    try

---
+++ demo1/Problem_2.py
@@ -0,0 +1,2 @@
+# We use Terinal to run Python scripts by using the command "python" for REPL
+# We can create a operation by using operators like +, -, *, /, etc. in Terinal 
---
+++ demo1/module.py
@@ -0,0 +1,7 @@
+import pyjokes
+# print("Printing Jokes by PyJokes")
+# For comment use '#' at the beginning of the line
+joke = pyjokes.get_joke()
+print(joke)
+"""Thx for giving the joke"""
+# For multiline cmments use triple quotes(""" """)
